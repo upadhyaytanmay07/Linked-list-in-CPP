24cec0b36d055a5dcc8de3f1fdbb0e0e39b64c6c
upadhyaytanmay07 <tanmayupadhyay1159@gmail.com>
2025-09-29 23:23:35 +0530

    Create linked list 2.cpp

---
+++ linked list 2.cpp
@@ -0,0 +1,40 @@
+/*Tanmay Upadhyay
+24070123120
+ENTC b2
+*/
+
+
+#include <iostream>
+using namespace std;
+
+class Node{
+    public:
+    int val;
+    Node* next;
+    
+    Node(int data){
+        val=data;
+        next=NULL;
+    }
+};
+
+int main(){
+    Node* n1= new Node(10);
+    Node* n2= new Node(20);
+    Node* n3= new Node(30);
+    
+    n1->next=n2;
+    n2->next=n3;
+    
+    Node* temp=n1;
+    while(temp!=NULL){
+        cout<<temp->val<<" ";
+        temp=temp->next;
+    }
+    cout<<endl;
+    return 0;
+}
+
+/*Output
+10 20 30
+*/
